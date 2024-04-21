3077a997b63dd4e51f54c958a4acf0107a7870d6
Luiza Leahu <leahuluiza@gmail.com>
2024-04-21 22:10:34 +0200

    back-end

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { TListItem } from "@/types/data"
+import { getAll, deleteItem } from '@/repository'
+
+function ListItem(item: TListItem) {
+  return <li>
+    <div>{item.id}</div>
+    <div>{item.applicationHostname}</div>
+    <div>{item.timestamp}</div>
+    <div>{item.type}</div>
+    <button>EDIT</button>
+    <button>X</button>
+
+  </li>
+}
+
+export default async function Home() {
+  const data = await getAll() as TListItem[]
+  console.log(data)
+  const listItems = data.map(item =>
+    <ListItem {...item} />
+  );
+
+  return (
+    <main>
+      <ul>{listItems}</ul>
+    </main>
+  );
+}
